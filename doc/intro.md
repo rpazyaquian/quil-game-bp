c6d6fe6b318c4376e5236ca506a97f3e29eb673f
Rebecca Paz <r.pazyaquian@gmail.com>
2015-08-19 21:10:18 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to basic-game-test
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/basic_game_test/core.clj
@@ -0,0 +1,7 @@
+(ns basic-game-test.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
