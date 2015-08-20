2f798db75994edc9eb085eb456e615346ed62e8e
Rebecca Paz <r.pazyaquian@gmail.com>
2015-08-19 22:28:15 -0400

    move draw functions to basic-game-test.draw

--- src/basic_game_test/draw.clj
+++ src/basic_game_test/draw.clj
@@ -1,3 +1,15 @@
 (ns basic-game-test.draw
   (:gen-class)
   (:require [quil.core :as q]))
+
+(defn draw-time [time]
+  (q/fill 0 0 0)
+  (q/text (str time) 10 10))
+
+(defn draw-time-delta [time-delta]
+  (q/fill 255 0 0)
+  (q/text (str time-delta) 10 30))
+
+(defn draw-state [state]
+  (draw-time (:time state))
+  (draw-time-delta (:time-delta state)))
