52dff9c64b5f050ee0cd3f05e71b860f40516ef1
Rebecca Paz <r.pazyaquian@gmail.com>
2015-08-19 21:18:23 -0400

    add entry point to program

--- src/basic_game_test/core.clj
+++ src/basic_game_test/core.clj
@@ -7,7 +7,15 @@
 (defn update [state])
 (defn draw [state])
 
+(defn make-sketch
+  (q/defsketch basic-game-test
+    :size [200 200]
+    :setup setup
+    :draw draw
+    :update update
+    :middleware [m/fun-mode]))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (make-sketch))
