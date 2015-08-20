c8788489c65e949e3f74ff69271d61aae0dab1bd
Rebecca Paz <r.pazyaquian@gmail.com>
2015-08-19 22:29:53 -0400

    privatize inner draw functions

--- src/basic_game_test/draw.clj
+++ src/basic_game_test/draw.clj
@@ -2,14 +2,14 @@
   (:gen-class)
   (:require [quil.core :as q]))
 
-(defn draw-time [time]
+(defn -draw-time [time]
   (q/fill 0 0 0)
   (q/text (str time) 10 10))
 
-(defn draw-time-delta [time-delta]
+(defn -draw-time-delta [time-delta]
   (q/fill 255 0 0)
   (q/text (str time-delta) 10 30))
 
 (defn draw-state [state]
-  (draw-time (:time state))
-  (draw-time-delta (:time-delta state)))
+  (-draw-time (:time state))
+  (-draw-time-delta (:time-delta state)))
