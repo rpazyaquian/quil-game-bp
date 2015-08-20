91ae94c617f89e23a72ed2fe7f32fd8a2c53b608
Rebecca Paz <r.pazyaquian@gmail.com>
2015-08-19 21:17:20 -0400

    define main sketch functions

--- src/basic_game_test/core.clj
+++ src/basic_game_test/core.clj
@@ -3,6 +3,10 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(defn setup [])
+(defn update [state])
+(defn draw [state])
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
