4660aeb34394b8b11dfe940b038608367afc333c
Rebecca Paz <r.pazyaquian@gmail.com>
2015-08-19 22:28:43 -0400

    remove draw functions from core

--- src/basic_game_test/core.clj
+++ src/basic_game_test/core.clj
@@ -3,18 +3,6 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
-(defn draw-time [time]
-  (q/fill 0 0 0)
-  (q/text (str time) 10 10))
-
-(defn draw-time-delta [time-delta]
-  (q/fill 255 0 0)
-  (q/text (str time-delta) 10 30))
-
-(defn draw-state [state]
-  (draw-time (:time state))
-  (draw-time-delta (:time-delta state)))
-
 (defn setup []
   (let [time (System/currentTimeMillis)]
     {:time time
